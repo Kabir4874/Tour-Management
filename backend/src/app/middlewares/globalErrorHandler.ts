2dca486f0e132e0652ca66019754fc8537ba06f5
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2026-01-31 19:15:02 +0600

    Kabir->[Done] update

--- backend/src/app/middlewares/globalErrorHandler.ts
+++ backend/src/app/middlewares/globalErrorHandler.ts
@@ -8,12 +8,39 @@ export const globalErrorHandler = (
   error: any,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   let statusCode = 500;
   let message = `Something went wrong!`;
+  const errorSources: any = [];
 
-  if (error instanceof AppError) {
+  if (error.code === 11000) {
+    const matchedArray = error.message.match(/"([^"]*)"/);
+    statusCode = 400;
+    message = `${matchedArray[1]} already exists!`;
+  } else if (error.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid MongoDB ObjectID. Please provide a valid ID";
+  } else if (error.name === "ZodError") {
+    statusCode = 400;
+    message = "Zod Error";
+    error.issues.forEach((issue: any) => {
+      errorSources.push({
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      });
+    });
+  } else if (error.name === "ValidationError") {
+    statusCode = 400;
+    const errors = Object.values(error.errors);
+    errors.forEach((errorObject: any) =>
+      errorSources.push({
+        path: errorObject.path,
+        message: errorObject.message,
+      }),
+    );
+    message = "Validation Error";
+  } else if (error instanceof AppError) {
     statusCode = error.statusCode;
     message = error.message;
   } else if (error instanceof Error) {
@@ -23,6 +50,7 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     error,
     stack: envVars.NODE_ENV === "development" ? error.stack : null,
   });
